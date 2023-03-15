2e2f8f56e80c2644abab07b4670bd209b3681f4b
disheng <ongdisheng@gmail.com>
2023-03-15 08:06:53 +0800

    Completed exercise 659

---
+++ 659-Encode-and-Decode-Strings/solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    """
+    Time: O(n)
+    Space: O(n)
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        res = ""
+
+        for s in strs:
+            res += str(len(s)) + "#" + s
+
+        return res
+        
+
+    """
+    Time: O(n)
+    Space: O(n)
+    @param: str: A string
+    @return: dcodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        res, i = [], 0
+
+        while i < len(str):
+            j = i
+            while str[j] != "#":
+                j += 1
+            length = int(str[i : j])
+            res.append(str[j+1 : j+1+length])
+            i = j+1+length
+
+        return res
